211daaefd098c4774c6369a5ff29099112f39c4b
Jean Souza <jean.vsouza@gmail.com>
2025-06-25 21:40:33 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,9 @@
 package com.example.workshopmongo.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongo.domain.Post;
-import com.example.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
